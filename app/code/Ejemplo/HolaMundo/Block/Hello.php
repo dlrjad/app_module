3b9dac294b5ea56935c4377fd63b7c6d66022b37
dlrjad <jorgdelga@hotmail.com>
2018-07-11 12:05:45 +0100

    Implemented Model

--- app/code/Ejemplo/HolaMundo/Block/Hello.php
+++ app/code/Ejemplo/HolaMundo/Block/Hello.php
@@ -3,17 +3,22 @@
 namespace Ejemplo\HolaMundo\Block;
 
 use Magento\Framework\View\Element\Template;
+use Ejemplo\HolaMundo\Model\ResourceModel\Item\CollectionFactory;
 
 class Hello extends Template
 {
-    public function __construct(Template\Context $context, 
-            array $data = []
-            ){
+    private $collectionFactory;
+    
+    public function __construct(Template\Context $context,
+        CollectionFactory $collectionFactory,
+        array $data = []
+        ){
+            $this->collectionFactory = $collectionFactory;
             parent::__construct($context, $data);
-    }
+        }
 
-    public function sayHello(){
-        return "Hola Mundo de nuestro modulo";
+    public function getItems(){
+        return $this->collectionFactory->create()->getItems();
     }
     
 }
